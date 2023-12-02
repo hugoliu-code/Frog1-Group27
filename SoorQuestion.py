f03d1b4b44f4b5a65f87890cd002fa901f9bc586
Soor1 <soorhansalia@icloud.com>
2023-12-02 12:13:59 -0500

    Soor ex.

---
+++ SoorQuestion.py
@@ -0,0 +1,23 @@
+import earthquakes
+import pandas as pd
+import matplotlib.pyplot as plt
+
+earthquake_data = earthquakes.get_earthquake()
+
+data_list = []
+
+for x in earthquake_data:
+    data = (x["impact"]["magnitude"], x["location"]["depth"])
+    data_list.append(data)
+
+columns = ["Magnitude", "Depth"]
+data_frame = pd.DataFrame(data_list, columns=columns)
+
+correlation = data_frame["Magnitude"].corr(data_frame["Depth"])
+print(correlation)
+
+plt.scatter(data_frame["Depth"], data_frame["Magnitude"])
+plt.title(f"Scatter Plot - Correlation: {correlation:.2f}")
+plt.xlabel("Depth (km)")
+plt.ylabel("Magnitude (Richter Scale)")
+plt.show()
--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 import earthquakes
 
-print(earthquakes.get_earthquake()[0])
+
+print(earthquakes.get_earthquake()[1])
+
+for x in earthquakes.get_earthquake():
+    print((x["impact"])["magnitude"], (x["location"])["depth"])
