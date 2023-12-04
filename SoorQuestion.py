959ac90cf712e0aaf698f843f504d75574c88a4b
Soor1 <128494314+Soor1@users.noreply.github.com>
2023-12-03 20:22:48 -0500

    Update SoorQuestion.py

--- SoorQuestion.py
+++ SoorQuestion.py
@@ -1,15 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.stats as stats
+import numpy as np
+
 
 loaded_df = pd.read_pickle('earthquake_data.pkl')
 
 data_frame = loaded_df[["magnitude", "depth"]]
 
 correlation = data_frame["magnitude"].corr(data_frame["depth"])
-print(correlation)
+sample_size = len(loaded_df)
 
-plt.scatter(data_frame["depth"], data_frame["magnitude"])
-plt.title(f"Scatter Plot - Correlation: {correlation:.2f}")
-plt.xlabel("Depth (km)")
-plt.ylabel("Magnitude (Richter Scale)")
+threshold = 0.75
+alpha = 0.01
+t_statistic = (correlation - threshold) / np.sqrt((1 - correlation**2) / (sample_size - 2))
+
+p_value = (1 - stats.t.cdf(abs(t_statistic), sample_size - 2))
+
+plt.scatter(data_frame["magnitude"], data_frame["depth"])
+plt.title(f"Scatter Plot - Correlation: {correlation:.3f}, p-value: {p_value:.4f}\nAlternative Hypothesis: $\\rho < 0.75$")
+plt.xlabel("Magnitude (Richter Scale)")
+plt.ylabel("Depth (km)")
 plt.show()
+
+
+print(f"Null Hypothesis: r <= {alpha}")
+print(f"Alternate Hypothesis:  r < {alpha}")
+print(f"Correlation: {correlation}")
+print(f"P-Value: {p_value}\n")
+
+if p_value < alpha:
+    print(f"Because the p-value is less than {alpha} we reject the hypothesis that r is greater than or equal to 0.75.")
+    print("There is a relatively weak correlation between earthquake magnitude and depth")
+else:
+    print(f"Because the p-value is greater than {alpha} we fail to reject the hypothesis that r is greater than or equal to 0.75.")
+    print("There is a strong correlation between earthquake magnitude and depth")
+
